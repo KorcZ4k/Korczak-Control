f05f07925f9514b326ce3925dbe05afb7bb92483
Korczak <contaparameulove@gmail.com>
2026-09-06 22:03:51 -0300

    Create Compose Desktop module

---
+++ android/KorczakControl/desktop/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.compose")
+}
+
+group = "com.korczak.control"
+version = "0.3.99"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.korczak.control.desktop.MainKt"
+        nativeDistributions {
+            targetFormats(
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg,
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi,
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb
+            )
+            packageName = "KorczakControl"
+            packageVersion = "0.3.99"
+        }
+    }
+}
